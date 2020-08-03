c0da1dc480cf70bb263027a223a54b75955301c4
Yago <yagorodi@gmail.com>
2020-08-03 01:11:48 -0300

    Clase 26 terminada

--- Objects/objConsole.js
+++ Objects/objConsole.js
@@ -20,4 +20,42 @@ console.groupEnd();
 console.table(Object.entries(console));
 console.table(Object.entries(console).sort()); //Ordeno alfabéticamente
 
+const numeros = [1,2,3,4,5];
+const vocales = ["a","e","i","o","u"];
 
+console.log("<---------------------------------------->");
+console.table(numeros);
+console.table(vocales);
+
+console.log("<---------------------------------------->");
+const perro = {
+    nombre:"Scott",
+    raza:"Callejero",
+    color:"Gris"
+}
+console.table(perro);
+
+console.log("<----PARA VER CUANTO TIEMPO TARDA EN EJECUTARSE UN BLOQUE DE CÓDIGO.---->");
+console.time("Cuánto tiempo tarda en ejecutarse mi código?");//ALIAS
+const arreglo = Array(1000000);
+
+for (let i = 0; i < arreglo.length; i++) {
+    arreglo[i] = i;
+}
+
+console.timeEnd("Cuánto tiempo tarda en ejecutarse mi código?");
+
+console.log("<---------------------------------------->");
+console.log("<------PARA CONTAR------>");
+for (let i = 0; i <= 100; i++) {
+    console.count("Código for:");
+    console.log("LOG: " + i);
+}
+
+console.log("<----UNIT TEST----->");
+let x = 2;
+let y = 2;
+let pruebaXY = "Se espera que X siempre sea menor que Y";
+
+//Si no aparece nada en la consola, es que se ejecutó correctamente.
+console.assert(x < y,{x,y,pruebaXY}); 
